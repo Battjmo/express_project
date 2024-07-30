ab79bd46260c517076b418abca520d554b67c394
Nick Battjes <nhbattjes@gmail.com>
2024-07-30 11:45:58 -0700

    hot reload on client works

--- main.ts
+++ main.ts
@@ -1,40 +1,22 @@
-// import express, { Express, Request, Response } from "express";
-// const dotenv = require("dotenv");
-
-// dotenv.config();
-
-// const app: Express = express();
-// const port = process.env.PORT || 3000;
-
-// app.set("view engine", "ejs");
-// app.use(express.static("public"));
-
-// app.get("/", (req: Request, res: Response) => {
-//   // get the user's name from the request
-//   res.send({ 'name': 'John Doe' });
-// });
-
-// app.listen(port, () => {
-//   console.log(`hi`);
-//   // render some more stuff:
-// });
-
-// src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+var reload = require("reload");
 
 dotenv.config();
 
 const app: Express = express();
-app.use(express.static(__dirname + "/public"));
 const port = process.env.PORT || 3000;
-// set the view engine to ejs
+
 app.set("view engine", "pug");
 
 app.get("/", (req: Request, res: Response) => {
-  res.render("index");
+  const title = "Home";
+  res.render("index", { title: title });
 });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log('carrots')
 });
+
+reload(app); // reload the server when the code changes
